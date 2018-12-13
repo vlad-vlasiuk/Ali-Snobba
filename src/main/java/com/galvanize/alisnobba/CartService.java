76117016a80a43847a78ec113ea8978e4e9ef913
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-13 12:23:05 -0700

    add incProduct() to service
    with two test (positive and negative)
    all test - red

--- src/main/java/com/galvanize/alisnobba/CartService.java
+++ src/main/java/com/galvanize/alisnobba/CartService.java
@@ -42,4 +42,7 @@ public class CartService {
 
     }
 
+    public void incProduct(Product product) {
+    }
+
 }
--- src/test/java/com/galvanize/alisnobba/CartServiceTest.java
+++ src/test/java/com/galvanize/alisnobba/CartServiceTest.java
@@ -1,19 +1,19 @@
 package com.galvanize.alisnobba;
 
-import com.galvanize.alisnobba.CartService;
 import com.galvanize.alisnobba.entity.Cart;
 import com.galvanize.alisnobba.entity.LineItem;
 import com.galvanize.alisnobba.entity.Product;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class CartServiceTest {
 
@@ -57,4 +57,66 @@ public class CartServiceTest {
         assertThat("expected that size cart is 0", service.getCart().getItems().size(), is(0));
 
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void incWrongProductTest() {
+        Cart cart = new Cart();
+        Product notFindProduct = new Product.ProductBuilder()
+                .id(3L)
+                .name("notFindProduct")
+                .price(new BigDecimal(0.50))
+                .build();
+
+        Product watch = new Product.ProductBuilder()
+                .id(1L)
+                .name("watch SuperWatch")
+                .price(new BigDecimal(5000.50))
+                .build();
+        LineItem lineItem1 = new LineItem(watch, 1);
+        Product someProduct = new Product.ProductBuilder()
+                .id(2L)
+                .name("someProduct")
+                .price(new BigDecimal(1.50))
+                .build();
+        LineItem lineItem2 = new LineItem(someProduct, 1);
+        cart.setItems(new ArrayList<>(Arrays.asList(lineItem1, lineItem2)));
+
+        service.setCart(cart);
+
+        service.incProduct(notFindProduct);
+
+
+    }
+
+    @Test
+    public void incProductTest() {
+        Cart cart = new Cart();
+        Product watch = new Product.ProductBuilder()
+                .id(1L)
+                .name("watch SuperWatch")
+                .price(new BigDecimal(5000.50))
+                .build();
+        LineItem lineItem1 = new LineItem(watch, 1);
+        Product someProduct = new Product.ProductBuilder()
+                .id(2L)
+                .name("someProduct")
+                .price(new BigDecimal(1.50))
+                .build();
+        LineItem lineItem2 = new LineItem(someProduct, 1);
+        cart.setItems(new ArrayList<>(Arrays.asList(lineItem1, lineItem2)));
+
+        service.setCart(cart);
+
+        service.incProduct(watch);
+
+        List<LineItem> items = service.getCart().getItems();
+
+        List<LineItem> collect = items.stream().filter(e -> e.getProduct().equals(watch)).collect(Collectors.toList());
+
+        assertThat(collect.size(), is(1));
+        assertThat(collect.get(0).getProduct(), is(watch));
+        assertThat(collect.get(0).getQuan(), is(2));
+
+    }
+
 }
